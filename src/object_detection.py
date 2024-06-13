b890e9b00ff2a1b808d44dd0ed44857be6a60d2c
anton <s224268@dtu.dk>
2024-06-13 10:42:42 +0200

    tilføjet opel corsa

--- src/object_detection.py
+++ src/object_detection.py
@@ -1,7 +1,20 @@
 import cv2
 import imutils
 import numpy as np
+from cv2.gapi.wip.draw import Circle
+
 from src.dto import CircleObject, SquareObject, Position
+
+
+def calculate_positive_angle(circle1: CircleObject, circle2: CircleObject) -> float:
+    pos1 = np.array([circle1.position.x, circle1.position.y])
+    pos2 = np.array([circle2.position.x, circle2.position.y])
+    delta = pos2 - pos1
+    angle = np.arctan2(delta[1], delta[0])
+    if angle < 0:
+        angle += 2 * np.pi
+    return angle
+
 
 class RoboVision:
     _whiteLower = np.array([0, 0, 220])
@@ -10,14 +23,47 @@ class RoboVision:
     _whiteSizeUpper = 20
     _eggSizeLower = _whiteSizeUpper
     _eggSizeUpper = 50
+    _dotSizeLower = 2
+    _dotSizeUpper = 1000000
 
+    #Outdated values
+    """
     _red1lower_limit = np.array([0, 100, 20])
     _red1upper_limit = np.array([10, 255, 255])
-    _red2lower_limit = np.array([160, 40, 20])
+    _red2lower_limit = np.array([160, 40, 20]) #TODO: Sat here should be higher ideally
     _red2upper_limit = np.array([179, 255, 255])
 
+    #_green_lower_limit = np.array([67, 70, 40])
+    #_green_upper_limit = np.array([97, 255, 255])
+    #_blue_lower_limit = np.array([98, 70, 40])
+    #_blue_upper_limit = np.array([145, 255, 255])
+    _green_lower_limit = np.array([55, 100, 70])
+    _green_upper_limit = np.array([80, 255, 255])
+    # Id like to avoid overlap in these filters
+    _blue_lower_limit = np.array([95, 180, 40])
+    _blue_upper_limit = np.array([115, 255, 255])
+    """
+
+    whiteLower = np.array([0, 0, 220])
+    whiteUpper = np.array([255, 100, 255])
+
+    #Theres two of these because red is where the circle of HSV "overlaps"
+    red1lower_limit = np.array([0, 125, 20])
+    red1upper_limit = np.array([10, 255, 255])
+    red2lower_limit = np.array([160, 125, 20])
+    red2upper_limit = np.array([179, 255, 255])
+
+    _green_lower_limit = np.array([55, 100, 70])
+    _green_upper_limit = np.array([80, 255, 255])
+    # Id like to avoid overlap in these filters
+    _blue_lower_limit = np.array([95, 180, 40])
+    _blue_upper_limit = np.array([115, 255, 255])
+
+    _orange_lower_limit = np.array([10, 100, 40])
+    _orange_upper_limit = np.array([25, 255, 255])
+
     _cross_area = 100
-    _vs = cv2.VideoCapture(1)
+    _vs = cv2.VideoCapture(0)
 
 
     #Variables to be updated every darn iteration
@@ -72,25 +118,47 @@ class RoboVision:
 
 
 
-    def getWhiteBalls(self):
+    def get_white_balls(self):
         return self._getBallishThing(self._whiteLower, self._whiteUpper, self._whiteSizeLower, self._whiteSizeUpper)
 
-    def getOrangeBall(self):
+    def get_orange_ball(self):
         return self._getBallishThing(self.todo, self.todo, self._whiteSizeLower, self._whiteSizeUpper)
 
-    def getEgg(self):
+    def get_egg(self):
         return self._getBallishThing(self._whiteLower, self._whiteUpper, self._eggSizeLower, self._eggSizeUpper)
 
-    #Returns a list of approximated poly DP, see https://docs.opencv.org/4.x/d3/dc0/group__imgproc__shape.html#ga0012a5fdaea70b8a9970165d98722b4c
-
-
+    def get_robot(self):
+        greendots = self._getBallishThing(self._green_lower_limit, self._green_upper_limit, self._dotSizeLower, self._dotSizeUpper)
+        if len(greendots) == 0:
+            raise Exception("No green dots detected")
+        elif len(greendots) > 1:
+            raise Exception("Multiple green dots detected")
+        greendot = greendots[0]
+        bluedots = self._getBallishThing(self._blue_lower_limit, self._blue_upper_limit, self._dotSizeLower, self._dotSizeUpper)
+        if len(bluedots) == 0:
+            raise Exception("No blue dots detected")
+        elif len(bluedots) > 1:
+            raise Exception("Multiple blue dots detected")
+        bluedot = bluedots[0]
+        center = CircleObject(radius=1,
+                              position=Position(x=int((greendot.position.x+bluedot.position.x)/2),
+                                                y=int((greendot.position.y+bluedot.position.y)/2)))
+        angle = calculate_positive_angle(greendot, bluedot)
+        print("Center: " + str(center) + "  Angle: " + str(angle))
+        return center, angle
 
 
 
 
 if __name__ == '__main__':
+
     robo = RoboVision()
-    whiteBalls = robo.getWhiteBalls()
+    while True:
+        try:
+            robo.get_robot()
+        except:
+            continue
+    whiteBalls = robo.get_white_balls()
     #orangeBalls = robo.getOrangeBall()
     cross = robo.getCross()
 
