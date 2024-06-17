5e74b89a3df4593093203a790eaa5a832f586946
Christoffer Foldager <christoffer@decuria.dk>
2024-06-17 16:09:48 +0200

    Ev3 Sends a recive message back when command is recived

--- src/transmission.py
+++ src/transmission.py
@@ -21,28 +21,28 @@ def prepare_command(move: Move):
     if move.radians == 0:
         lm = (speed/2)
         rm = (speed/2)
-    elif abs(move.radians) == 0.025:
-        if move.radians < 0:
-            lm = (speed/2) * 1
-            rm = (speed/2) * 0.75
-        else:
-            lm = (speed/2) * 0.75
-            rm = (speed/2) * 1
-    elif abs(move.radians) == 0.05:
+    elif abs(move.radians) > 0.02 and abs(move.radians) < 0.03:
         if move.radians < 0:
             lm = (speed/2) * 1
             rm = (speed/2) * 0.5
         else:
             lm = (speed/2) * 0.5
             rm = (speed/2) * 1
-    elif abs(move.radians) == 0.075:
+    elif abs(move.radians) > 0.04 and abs(move.radians) < 0.06:
         if move.radians < 0:
             lm = (speed/2) * 1
             rm = (speed/2) * 0.25
         else:
             lm = (speed/2) * 0.25
             rm = (speed/2) * 1
-    elif abs(move.radians) == 0.1:
+    elif abs(move.radians) > 0.07 and abs(move.radians) < 0.08:
+        if move.radians < 0:
+            lm = (speed/2) * 1
+            rm = (speed/2) * -1
+        else:
+            lm = (speed/2) * -1
+            rm = (speed/2) * 1
+    elif abs(move.radians) > 0.09 and abs(move.radians) < 0.11:
         if move.radians < 0:
             lm = (speed/2) * 1
             rm = (speed/2) * -1
@@ -56,6 +56,7 @@ def prepare_command(move: Move):
 def send_command(move: Move):
     command = prepare_command(move)
     command_string = f"{command[0]} {command[1]} {command[2]} {command[3]}" #Left motor, Right motor, Fan, latch
+    print(f'command string: {command_string}')
     client_socket.send(command_string.encode())
-
+    client_socket.recv(1024)
 
