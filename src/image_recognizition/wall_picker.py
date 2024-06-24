09ed1f2ef1b5a5122a820dffb153da53e09f2318
anton <s224268@dtu.dk>
2024-06-24 20:07:34 +0200

    Fixed pick_hole and gitignore

--- src/image_recognizition/wall_picker.py
+++ src/image_recognizition/wall_picker.py
@@ -74,77 +74,102 @@ class WallPicker:
             if len(self.points) >= self.max_points:
                 cv2.destroyAllWindows()
 
-    def _pick_points(self, window_name, num_points) -> SquareObject:
-        self.points = []
-        self.max_points = num_points
-        self.frame_name = window_name
-        cv2.namedWindow(self.frame_name)
-        cv2.setMouseCallback(self.frame_name, self._click_event)
+    def _pick_four_points(self, window_name, num_points) -> SquareObject:
+        if num_points == 4:
+            self.points = []
+            self.max_points = num_points
+            self.frame_name = window_name
+            cv2.namedWindow(self.frame_name)
+            cv2.setMouseCallback(self.frame_name, self._click_event)
 
 
-        while True:
+            while True:
 
-            ret, self.frame = self.cap.read()
-            cv2.imshow(self.frame_name, self.frame)
+                ret, self.frame = self.cap.read()
+                cv2.imshow(self.frame_name, self.frame)
 
-            if not ret:
-                print("Error: Could not read frame.")
-                break
+                if not ret:
+                    print("Error: Could not read frame.")
+                    break
 
-            if cv2.waitKey(1) & 0xFF == ord('q') or len(self.points) >= num_points:
-                break
-        print(self.points)
+                if cv2.waitKey(1) & 0xFF == ord('q') or len(self.points) >= num_points:
+                    break
+            print(self.points)
 
-        p1 = CircleObject(radius=1, position=Position(x=self.points[0][0], y=self.points[0][1]))
-        p2 = CircleObject(radius=1, position=Position(x=self.points[1][0], y=self.points[1][1]))
-        angle = calculate_positive_angle(p1, p2)
-        width = int(euclidean_distance(self.points[0], self.points[1]))
-        height = int(euclidean_distance(self.points[1], self.points[2]))
-        x, y = calculate_centroid(self.points)
-        print(x,y)
+            p1 = CircleObject(radius=1, position=Position(x=self.points[0][0], y=self.points[0][1]))
+            p2 = CircleObject(radius=1, position=Position(x=self.points[1][0], y=self.points[1][1]))
+            angle = calculate_positive_angle(p1, p2)
+            width = int(euclidean_distance(self.points[0], self.points[1]))
+            height = int(euclidean_distance(self.points[1], self.points[2]))
+            x, y = calculate_centroid(self.points)
+            print(x,y)
 
 
 
-        return SquareObject.create_square(
-                            position=Position(x=x, y=y),
-                            width=width,
-                            height=height,
-                            radians=angle,
-                            offset_x=0,
-                            offset_y=0)
+            return SquareObject.create_square(
+                                position=Position(x=x, y=y),
+                                width=width,
+                                height=height,
+                                radians=angle,
+                                offset_x=0,
+                                offset_y=0)
+
+        else:
+            raise ValueError("Only 4 implemented ")
 
     def pick_north_wall(self):
         print("Click 4 points for the North Wall")
-        square = self._pick_points("North", 4)
+        square = self._pick_four_points("North", 4)
         wall = Wall.create(square, WallPlacement.TOP)
         return wall
 
     def pick_east_wall(self):
         print("Click 4 points for the East Wall")
-        square = self._pick_points("East", 4)
+        square = self._pick_four_points("East", 4)
         wall = Wall.create(square, WallPlacement.LEFT)
         return wall
 
     def pick_south_wall(self):
         print("Click 4 points for the South Wall")
-        square = self._pick_points("South", 4)
+        square = self._pick_four_points("South", 4)
         wall = Wall.create(square, WallPlacement.BOT)
         return wall
     def pick_west_wall(self):
         print("Click 4 points for the West Wall")
-        square = self._pick_points("West", 4)
+        square = self._pick_four_points("West", 4)
         wall = Wall.create(square, WallPlacement.RIGHT)
         return wall
 
     def _click_cross_one(self):
         print("Click 4 points for Cross One")
-        return self._pick_points("One rectangle from cross", 4)
+        return self._pick_four_points("One rectangle from cross", 4)
 
     def pick_hole(self):
         print("Click the two of the hole")
-        points = self._pick_points("Hole", 2)
-        x, y = calculate_centroid(points)
-        return CircleObject(radius=1, position=Position(x=x, y=y))
+
+        self.points = []
+        self.max_points = 2
+        self.frame_name = "goal hole"
+        cv2.namedWindow("goal hole")
+        cv2.setMouseCallback("goal hole", self._click_event)
+
+        while True:
+
+            ret, self.frame = self.cap.read()
+            cv2.imshow(self.frame_name, self.frame)
+            if not ret:
+                print("Error: Could not read frame.")
+                break
+
+            if cv2.waitKey(1) & 0xFF == ord('q') or len(self.points) >= 2:
+                break
+
+        # Very python
+        p1 = CircleObject(radius=1, position=Position(x=self.points[0][0], y=self.points[0][1]))
+        p2 = CircleObject(radius=1, position=Position(x=self.points[1][0], y=self.points[1][1]))
+        x,y = calculate_centroid([(p1.position.x, p1.position.y), (p2.position.x, p2.position.y)])
+        circle = CircleObject(radius=1, position=Position(x=x,y=y))
+        return circle
 
     def pick_cross(self):
         cross_part_one: SquareObject = self._click_cross_one()
@@ -158,6 +183,8 @@ class WallPicker:
     def release(self):
         self.cap.release()
         cv2.destroyAllWindows()
+
+
 
 
 if __name__ == "__main__":
